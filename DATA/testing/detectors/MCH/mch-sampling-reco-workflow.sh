aeded9c6a0c6af9fafbc36ee72728ea2c8665d1d
Laurent Aphecetche <laurent.aphecetche@cern.ch>
2022-04-14 14:27:14 +0200

    MCH: sampling reco with time clustering and preclustering

--- DATA/testing/detectors/MCH/mch-sampling-reco-workflow.sh
+++ DATA/testing/detectors/MCH/mch-sampling-reco-workflow.sh
@@ -19,9 +19,20 @@ PROXY_INSPEC="A:MCH/RAWDATA;B:FLP/DISTSUBTIMEFRAME/0"
 CONSUL_ENDPOINT="alio2-cr1-hv-aliecs.cern.ch:8500"
 
 WORKFLOW="o2-dpl-raw-proxy ${ARGS_ALL} --dataspec \"$PROXY_INSPEC\" --channel-config \"name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=0\" | "
-WORKFLOW+="o2-mch-raw-to-digits-workflow ${ARGS_ALL} --configKeyValues \"MCHCoDecParam.sampaBcOffset=31;HBFUtils.nHBFPerTF=128\" --time-reco-mode bcreset | "
-WORKFLOW+="o2-qc $ARGS_ALL --config consul-json://${CONSUL_ENDPOINT}/o2/components/qc/ANY/any/mch-qcmn-epn-digits-expert | "
-WORKFLOW+="o2-dpl-run ${ARGS_ALL} ${GLOBALDPLOPT}"
+WORKFLOW+="o2-mch-raw-to-digits-workflow ${ARGS_ALL} --pipeline mch-data-decoder:4 --configKeyValues \"MCHCoDecParam.sampaBcOffset=31;HBFUtils.nHBFPerTF=128\" --time-reco-mode bcreset | "
+
+WORKFLOW+="o2-datasampling-standalone ${ARGS_ALL} --config json://$O2DPG_ROOT/DATA/testing/detectors/MCH/datasampling.json | "
+
+WORKFLOW+="o2-mch-digits-filtering-workflow ${ARGS_ALL} --input-digits-data-description \"S-DIGITS\" --input-digitrofs-data-description \"S-DIGITROFS\" --disable-mc true | "
+
+WORKFLOW+="o2-mch-digits-to-timeclusters-workflow ${ARGS_ALL} --input-digits-data-description \"F-DIGITS\" --input-digitrofs-data-description \"F-DIGITROFS\" --mch-debug | "
+
+WORKFLOW+="o2-mch-digits-to-preclusters-workflow ${ARGS_ALL} --check-no-leftover-digits off | "
+
+WORKFLOW+="o2-qc $ARGS_ALL --config consul-json://${CONSUL_ENDPOINT}/o2/components/qc/ANY/any/mch-qcmn-epn-digits-expert-direct | "
+
+WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
+
 
 if [ $WORKFLOWMODE == "print" ]; then
   echo Workflow command:
