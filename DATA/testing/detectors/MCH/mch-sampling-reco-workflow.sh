16f6e03a9034daa09aa7c59e45ec8a87cece79df
Laurent Aphecetche <laurent.aphecetche@cern.ch>
2022-05-04 12:16:20 +0200

    wip: playing with clustering lowestPadCharge param

--- DATA/testing/detectors/MCH/mch-sampling-reco-workflow.sh
+++ DATA/testing/detectors/MCH/mch-sampling-reco-workflow.sh
@@ -2,34 +2,25 @@
 
 source common/setenv.sh
 
-# ---------------------------------------------------------------------------------------------------------------------
-# Set general arguments
-ARGS_ALL="--session default --severity $SEVERITY --shm-segment-size $SHMSIZE $ARGS_ALL_EXTRA"
-ARGS_ALL+=" --infologger-severity $INFOLOGGER_SEVERITY"
-ARGS_ALL+=" --monitoring-backend influxdb-unix:///tmp/telegraf.sock --resources-monitoring 60"
-if [ $SHMTHROW == 0 ]; then
-  ARGS_ALL+=" --shm-throw-bad-alloc 0"
-fi
-if [ $NORATELOG == 1 ]; then
-  ARGS_ALL+=" --fairmq-rate-logging 0"
-fi
-ARGS_ALL_CONFIG="NameConf.mDirGRP=$FILEWORKDIR;NameConf.mDirGeom=$FILEWORKDIR;NameConf.mDirCollContext=$FILEWORKDIR;NameConf.mDirMatLUT=$FILEWORKDIR;keyval.input_dir=$FILEWORKDIR;keyval.output_dir=/dev/null;$ALL_EXTRA_CONFIG"
+source common/getCommonArgs.sh
 
 PROXY_INSPEC="A:MCH/RAWDATA;B:FLP/DISTSUBTIMEFRAME/0"
 CONSUL_ENDPOINT="alio2-cr1-hv-aliecs.cern.ch:8500"
 
 WORKFLOW="o2-dpl-raw-proxy ${ARGS_ALL} --dataspec \"$PROXY_INSPEC\" --channel-config \"name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=0\" | "
-WORKFLOW+="o2-mch-raw-to-digits-workflow ${ARGS_ALL} --pipeline mch-data-decoder:4 --configKeyValues \"MCHCoDecParam.sampaBcOffset=31;HBFUtils.nHBFPerTF=128\" --time-reco-mode bcreset | "
+WORKFLOW+="o2-mch-raw-to-digits-workflow ${ARGS_ALL} --pipeline mch-data-decoder:12 --configKeyValues \"${ARGS_ALL_CONFIG};MCHCoDecParam.sampaBcOffset=31;HBFUtils.nHBFPerTF=128\" --time-reco-mode bcreset | "
 
 WORKFLOW+="o2-datasampling-standalone ${ARGS_ALL} --config json://$O2DPG_ROOT/DATA/testing/detectors/MCH/datasampling.json | "
 
 WORKFLOW+="o2-mch-digits-filtering-workflow ${ARGS_ALL} --input-digits-data-description \"S-DIGITS\" --input-digitrofs-data-description \"S-DIGITROFS\" --disable-mc true | "
-
-WORKFLOW+="o2-mch-digits-to-timeclusters-workflow ${ARGS_ALL} --input-digits-data-description \"F-DIGITS\" --input-digitrofs-data-description \"F-DIGITROFS\" --mch-debug | "
-
+WORKFLOW+="o2-mch-digits-to-timeclusters-workflow ${ARGS_ALL} --only-trackable --input-digits-data-description \"F-DIGITS\" --input-digitrofs-data-description \"F-DIGITROFS\" --mch-debug | "
 WORKFLOW+="o2-mch-digits-to-preclusters-workflow ${ARGS_ALL} --check-no-leftover-digits off | "
 
-WORKFLOW+="o2-qc $ARGS_ALL --config consul-json://${CONSUL_ENDPOINT}/o2/components/qc/ANY/any/mch-qcmn-epn-digits-expert-direct | "
+WORKFLOW+="o2-mch-preclusters-to-clusters-original-workflow ${ARGS_ALL} --configKeyValues \"${ARGS_ALL_CONFIG};MCHClustering.defaultClusterResolution=0.4;MCHClustering.lowestPadCharge=100.\" | "
+WORKFLOW+="o2-mch-clusters-transformer-workflow ${ARGS_ALL} --configKeyValues \"${ARGS_ALL_CONFIG}\" | "
+WORKFLOW+="o2-mch-clusters-to-tracks-workflow ${ARGS_ALL} --configKeyValues \"${ARGS_ALL_CONFIG};MCHTracking.chamberResolutionX=0.1;MCHTracking.chamberResolutionY=0.1;MCHTracking.sigmaCutForTracking=3.;MCHTracking.sigmaCutForImprovement=3.\" | "
+
+#WORKFLOW+="o2-qc $ARGS_ALL --config consul-json://${CONSUL_ENDPOINT}/o2/components/qc/ANY/any/mch-qcmn-epn-digits-expert-direct | "
 
 WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
